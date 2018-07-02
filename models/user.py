dc41db07c4fd57a7ebc574689051c3e6e0b5e8d5
pngg <gathegu.p@gmail.com>
2018-07-02 13:20:50 +0300

    Add postgresql connection

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
-import datetime 
+import datetime
+form db import connect
 
 class User:
     def __init__(self, username, role, last_login, password):
@@ -9,4 +10,5 @@ class User:
     
     def username(self):
         return self.username
-        
+
+    def create_comment(self, message, author, created_at, updated_at, updated_by=None, parent=None):
