24876ba55800f802555adf83feedf62d909b883b
Mutuba <dmutuba@gmail.com>
2018-07-02 12:35:40 +0300

    [Feature-#158757958] Created registration function

---
+++ auth/reg.py
@@ -0,0 +1,16 @@
+from users import User
+import sys
+
+def reg():
+	user=User()
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    role = input("Enter your role")
+
+    user.add['username'] = username
+    user.add['password'] = password
+    user.add['role'] = role
+
+    return "User details added"
+    
+reg()
