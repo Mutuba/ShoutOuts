f8e5279edc5741068d2e068dd5dd5b4fa310b913
Mutuba <dmutuba@gmail.com>
2018-07-02 13:30:34 +0300

    Updated login to enable adding comment if logged in

--- auth/sign_in.py
+++ auth/sign_in.py
@@ -1,14 +1,24 @@
 from user import User
+from models.comment import Comment
 import sys
+import datetime
 
 def login():
 	users = User()
+	 
     username = input("Enter your username: ")
     password = input("Enter your password: ")
 
     for user in users:
         if user == username:
             return "Welcome, you are logged in"
+        comments = Comment()
+        parent = input("Enter parent comment")
+        message = input("Enter comment message")
+        created_at = str(datetime.now()) 
+        updated_at = str(datetime.now())
+        updated_by = user
+        author = user
         else:
             return "wrong password"
 
